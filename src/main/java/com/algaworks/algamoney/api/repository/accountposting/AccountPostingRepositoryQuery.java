0b7e05a225811b710396a45daa812dca65c26d7c
RicardoPolido <ricardo.polido@outlook.com>
2020-06-17 09:12:22 -0300

    Implementação de paginação na consulta de Lançamentos (AccountPosting)

--- src/main/java/com/algaworks/algamoney/api/repository/accountposting/AccountPostingRepositoryQuery.java
+++ src/main/java/com/algaworks/algamoney/api/repository/accountposting/AccountPostingRepositoryQuery.java
@@ -2,10 +2,10 @@ package com.algaworks.algamoney.api.repository.accountposting;
 
 import com.algaworks.algamoney.api.model.AccountPosting;
 import com.algaworks.algamoney.api.repository.filter.AccountPostingFilter;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AccountPostingRepositoryQuery {
 
-    public List<AccountPosting> filter(AccountPostingFilter accountPostingFilter);
+    public Page<AccountPosting> filter(AccountPostingFilter accountPostingFilter, Pageable pageable);
 }
